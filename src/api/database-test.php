17af718f840764bfb1b1370710b6ea43740a5247
Kumi <git@kumi.email>
2025-03-07 11:05:59 +0100

    feat: Prevent usage of database-test API after installation

--- src/api/database-test.php
+++ src/api/database-test.php
@@ -14,17 +14,23 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
         $success = false;
 
-        try {
-            $dsn = 'mysql:host=' . $_POST['DATABASE_HOST'] . ';dbname=' . $_POST['DATABASE_NAME'] . ';port=3306;charset=utf8';
-            $pdo = new \PDO(
-                $dsn,
-                $_POST['DATABASE_USER'],
-                $_POST['DATABASE_PASSWORD']
-            );
+        if (isset($database) && $database) {
+            echo _('Refused to test database connection after installation.');
+            $response['dbTestSuccess'] = false;
+        }
+        else {
+            try {
+                $dsn = 'mysql:host=' . $_POST['DATABASE_HOST'] . ';dbname=' . $_POST['DATABASE_NAME'] . ';port=3306;charset=utf8';
+                $pdo = new \PDO(
+                    $dsn,
+                    $_POST['DATABASE_USER'],
+                    $_POST['DATABASE_PASSWORD']
+                );
 
-            $success = true;
-        } catch (\Throwable $th) {
-            echo $th->getMessage();
+                $success = true;
+            } catch (\Throwable $th) {
+                echo $th->getMessage();
+            }
         }
 
         $response['dbTestSuccess'] = $success;
